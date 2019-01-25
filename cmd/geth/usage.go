b42b51857cf59936262d63970d1aa1869dc43389
Edwin <edwin@am.is>
2019-01-25 14:26:35 +0800

    *: add broadcast local txs feature

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -96,6 +96,7 @@ var AppHelpFlagGroups = []flags.FlagGroup{
 			utils.TxPoolNoLocalsFlag,
 			utils.TxPoolJournalFlag,
 			utils.TxPoolRejournalFlag,
+			utils.TxPoolBroadcastPendingLocalTxFlag,
 			utils.TxPoolPriceLimitFlag,
 			utils.TxPoolPriceBumpFlag,
 			utils.TxPoolAccountSlotsFlag,
