d286cba762350c45884679bb798d7306530a2e57
mark.lin <mark@maicoin.com>
2019-08-20 14:25:02 +0800

    *: add GetTransferLogs/GetTotalDifficulty/GetBlockReceipts/SubscribeNewQueuedTxsEvent apis

--- eth/api.go
+++ eth/api.go
@@ -387,6 +387,18 @@ func (api *DebugAPI) AccountRange(blockNrOrHash rpc.BlockNumberOrHash, start hex
 	return stateDb.IteratorDump(opts), nil
 }
 
+// GetTransferLogs is a debug API function that returns the transfer logs for a block hash, if known.
+func (api *PrivateDebugAPI) GetTransferLogs(ctx context.Context, hash common.Hash) ([]*types.TransferLog, error) {
+	number := rawdb.ReadHeaderNumber(api.eth.ChainDb(), hash)
+	if number == nil {
+		return nil, errors.New("unknown transfer logs")
+	}
+	if transferLogs := rawdb.ReadTransferLogs(api.eth.ChainDb(), hash, *number); transferLogs != nil {
+		return transferLogs, nil
+	}
+	return nil, errors.New("unknown transfer logs")
+}
+
 // StorageRangeResult is the result of a debug_storageRangeAt API call.
 type StorageRangeResult struct {
 	Storage storageMap   `json:"storage"`
@@ -439,6 +451,11 @@ func storageRangeAt(st state.Trie, start []byte, maxResult int) (StorageRangeRes
 		result.NextKey = &next
 	}
 	return result, nil
+}
+
+// GetTotalDifficulty returns the total difficulty of the specified block.
+func (api *PrivateDebugAPI) GetTotalDifficulty(blockHash common.Hash) *big.Int {
+	return api.eth.blockchain.GetTd(blockHash, api.eth.blockchain.GetBlockByHash(blockHash).NumberU64())
 }
 
 // GetModifiedAccountsByNumber returns all accounts that have changed between the
@@ -583,4 +600,9 @@ func (api *DebugAPI) GetAccessibleState(from, to rpc.BlockNumber) (uint64, error
 		}
 	}
 	return 0, fmt.Errorf("No state found")
+}
+
+// GetBlockReceipts returns all transaction receipts of the specified block.
+func (api *PrivateDebugAPI) GetBlockReceipts(blockHash common.Hash) types.Receipts {
+	return api.eth.blockchain.GetReceiptsByHash(blockHash)
 }
